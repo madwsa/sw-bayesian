2e88cd0e346b76af7da5e8ad51c0addcc0a9af43
madwsa <wsa@maia.wsa.dynu.net>
2019-02-06 07:40:28 -0600

    added bg

--- bg-stan-model.R
+++ bg-stan-model.R
@@ -9,6 +9,8 @@ mod_04_multilevel_planet <-
     brm(total_claims ~ (1 | planet) + pilot + vehicle +
             (0 + pilot + vehicle | planet),
         data = train, family = poisson(link = 'log'),
-        cores = 2, chains = 2, iter = 500)
+        prior = prior(normal(0, 5)),
+        control = list(adapt_delta = 0.85),
+        cores = 2, chains = 2, iter = 2000)
 
 saveRDS(mod_04_multilevel_planet, './data/mod_04_multilevel_planet_bg.rds')
